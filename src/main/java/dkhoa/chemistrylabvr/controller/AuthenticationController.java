b11dc60045dc8deed734fcafe3abd9dc58045edd
DangKhoaK4 <zuki101004@gmail.com>
2026-09-11 15:20:59 +0700

    base code

--- src/main/java/dkhoa/chemistrylabvr/controller/AuthenticationController.java
+++ src/main/java/dkhoa/chemistrylabvr/controller/AuthenticationController.java
@@ -1,8 +1,22 @@
 package dkhoa.chemistrylabvr.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import dkhoa.chemistrylabvr.dto.request.LoginRequestDTO;
+import dkhoa.chemistrylabvr.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@RequestMapping("/api/auth/")
+@Tag(name = "Authentication API", description = "Login, register, logout")
+@CrossOrigin("*")
 public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("login")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO dto) {
+        return ResponseEntity.ok(authenticationService.login(dto));
+    }
 }
