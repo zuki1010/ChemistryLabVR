c9d057420ca24c88eb35686615c947029eda9fdc
DangKhoaK4 <zuki101004@gmail.com>
2026-09-11 11:09:12 +0700

    .

---
+++ src/main/java/dkhoa/chemistrylabvr/ChemistryLabVrApplication.java
@@ -0,0 +1,13 @@
+package dkhoa.chemistrylabvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryLabVrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChemistryLabVrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dkhoa/chemistrylabvr/ChemistryLabVrApplicationTests.java
@@ -0,0 +1,13 @@
+package dkhoa.chemistrylabvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChemistryLabVrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
